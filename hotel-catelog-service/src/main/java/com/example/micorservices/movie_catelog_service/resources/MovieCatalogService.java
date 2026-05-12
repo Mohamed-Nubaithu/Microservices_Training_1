9714ad0ee5df1f95725508acd7f34aee627996d4
Mohamed Nubaithu <mdnubaithu123@gmail.com>
2026-05-12 07:26:40 +0530

    Added separate fallbacks for each external api calls

--- hotel-catelog-service/src/main/java/com/example/micorservices/movie_catelog_service/resources/MovieCatalogService.java
+++ hotel-catelog-service/src/main/java/com/example/micorservices/movie_catelog_service/resources/MovieCatalogService.java
@@ -2,6 +2,7 @@ package com.example.micorservices.movie_catelog_service.resources;
 
 import com.example.micorservices.movie_catelog_service.models.CatalogItem;
 import com.example.micorservices.movie_catelog_service.models.Movie;
+import com.example.micorservices.movie_catelog_service.models.Rating;
 import com.example.micorservices.movie_catelog_service.models.UserRating;
 import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,22 +27,47 @@ public class MovieCatalogService {
     private WebClient.Builder webClientBuilder;
 
     @RequestMapping("/{userId}")
-    @HystrixCommand(fallbackMethod = "getFallbackCatalog")
+//    @HystrixCommand(fallbackMethod = "getFallbackCatalog")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId)
     {
-        UserRating userRating = restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/"+userId, UserRating.class);
-        return userRating.getUserRating().stream().map(rating -> {
-            Movie movie = restTemplate.getForObject("http://movie-info-service/movies/"+rating.getMovieId(), Movie.class );
-            return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
-        }).collect(Collectors.toList());
+        UserRating userRating = getUserRating(userId);
+        return userRating.getUserRating().stream().map(this::getCatalogItem).collect(Collectors.toList());
     }
 
-    public List<CatalogItem> getFallbackCatalog(String userId, Throwable ex)
+//    public List<CatalogItem> getFallbackCatalog(String userId, Throwable ex)
+//    {
+//        System.out.println("Reason for --->"+ ex);
+//        ex.printStackTrace();
+//        return List.of(new CatalogItem("No Movie", "", 0));
+//    }
+
+    @HystrixCommand(fallbackMethod = "getFallbackUserRating")
+    public UserRating getUserRating(String userId)
     {
-        System.out.println("Reason for --->"+ ex);
-        ex.printStackTrace();
-        return List.of(new CatalogItem("No Movie", "", 0));
+        return restTemplate.getForObject("http://ratings-data-service/ratingsdata/user/"+userId, UserRating.class);
     }
+
+    public UserRating getFallbackUserRating(String userId)
+    {
+        UserRating userRating = new UserRating();
+        userRating.setUserId(userId);
+        userRating.setUserRating(List.of(new Rating("0", 0)));
+        return userRating;
+    }
+
+    @HystrixCommand(fallbackMethod = "getFallbackCatalog")
+    public CatalogItem getCatalogItem(Rating rating)
+    {
+        Movie movie = restTemplate.getForObject("http://movie-info-service/movies/"+rating.getMovieId(), Movie.class );
+        return new CatalogItem(movie.getName(), movie.getDescription(), rating.getRating());
+    }
+
+    public CatalogItem getFallbackCatalog(Rating rating)
+    {
+        return new CatalogItem("No Movie found", "", 0);
+    }
+
+
 }
 
 // using web client to make external api call
--- ratings-data-service/src/main/java/com/example/micorservices/ratings_data_service/models/UserRating.java
+++ ratings-data-service/src/main/java/com/example/micorservices/ratings_data_service/models/UserRating.java
@@ -3,7 +3,16 @@ package com.example.micorservices.ratings_data_service.models;
 import java.util.List;
 
 public class UserRating {
+    private String userId;
     private List<Rating> userRating;
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
 
     public List<Rating> getUserRating() {
         return userRating;
